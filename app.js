ff7fdf68a4aa58ae9bddfb38fe6fd7ceda199c64
Nithin23106 <nithingannu1465@gmail.com>
2025-11-03 00:14:15 +0000

    updated lab

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 function generateUserList(users, stocks) {
   const userList = document.querySelector('.user-list');
   userList.innerHTML = '';
-  users.map(({ user, id }) => {
+  users.forEach(({ user, id }) => {
     const listItem = document.createElement('li');
-    listItem.innerText = user.lastname + ', ' + user.firstname;
-    listItem.setAttribute('id', id);
+    listItem.innerText = `${user.lastname}, ${user.firstname}`;
+    listItem.id = id;
     userList.appendChild(listItem);
   });
-
   userList.addEventListener('click', (event) => handleUserListClick(event, users, stocks));
 }
 
 function handleUserListClick(event, users, stocks) {
   const userId = event.target.id;
-  const user = users.find(user => user.id == userId);
+  const user = users.find(u => u.id == userId);
   populateForm(user);
   renderPortfolio(user, stocks);
 }
@@ -32,19 +31,27 @@ function renderPortfolio(user, stocks) {
   const { portfolio } = user;
   const portfolioDetails = document.querySelector('.portfolio-list');
   portfolioDetails.innerHTML = '';
-  portfolio.map(({ symbol, owned }) => {
+  
+  portfolio.forEach(({ symbol, owned }) => {
+    const container = document.createElement('div');
+    container.className = 'portfolio-item';
+    
     const symbolEl = document.createElement('p');
-    const sharesEl = document.createElement('p');
-    const actionEl = document.createElement('button');
     symbolEl.innerText = symbol;
+    
+    const sharesEl = document.createElement('p');
     sharesEl.innerText = owned;
+    
+    const actionEl = document.createElement('button');
     actionEl.innerText = 'View';
-    actionEl.setAttribute('id', symbol);
-    portfolioDetails.appendChild(symbolEl);
-    portfolioDetails.appendChild(sharesEl);
-    portfolioDetails.appendChild(actionEl);
+    actionEl.id = symbol;
+    
+    container.appendChild(symbolEl);
+    container.appendChild(sharesEl);
+    container.appendChild(actionEl);
+    portfolioDetails.appendChild(container);
   });
-
+  
   portfolioDetails.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
       viewStock(event.target.id, stocks);
@@ -55,50 +62,7 @@ function renderPortfolio(user, stocks) {
 function viewStock(symbol, stocks) {
   const stockArea = document.querySelector('.stock-form');
   if (stockArea) {
-    const stock = stocks.find(function (s) { return s.symbol == symbol; });
-
+    const stock = stocks.find(s => s.symbol == symbol);
     document.querySelector('#stockName').textContent = stock.name;
     document.querySelector('#stockSector').textContent = stock.sector;
-    document.querySelector('#stockIndustry').textContent = stock.subIndustry;
-    document.querySelector('#stockAddress').textContent = stock.address;
-
-    document.querySelector('#logo').src = `logos/${symbol}.svg`;
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const stocksData = JSON.parse(stockContent);
-  const userData = JSON.parse(userContent);
-
-  generateUserList(userData, stocksData);
-
-  const deleteButton = document.querySelector('#delete');
-  const saveButton = document.querySelector('#save');
-
-  deleteButton.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    const userId = document.querySelector('#userID').value;
-    const userIndex = userData.findIndex(user => user.id == userId);
-    userData.splice(userIndex, 1);
-    generateUserList(userData, stocksData);
-  });
-
-  saveButton.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    const id = document.querySelector('#userID').value;
-
-    for (let i = 0; i < userData.length; i++) {
-      if (userData[i].id == id) {
-        userData[i].user.firstname = document.querySelector('#firstname').value;
-        userData[i].user.lastname = document.querySelector('#lastname').value;
-        userData[i].user.address = document.querySelector('#address').value;
-        userData[i].user.city = document.querySelector('#city').value;
-        userData[i].user.email = document.querySelector('#email').value;
-
-        generateUserList(userData, stocksData);
-      }
-    }
-  });
-});
+    document.querySelector('#stockIndustry'
